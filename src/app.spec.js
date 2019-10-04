6bd0ed8b939ea0aeb05810a35738d59db80523f3
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-04 12:23:17 +0300

    Clean-up.

--- src/app.spec.js
+++ src/app.spec.js
@@ -1,11 +1,11 @@
 /* global describe, beforeAll, afterAll, beforeEach, it, expect */
-import buildApp from './app';
-const request = require('supertest');
+// import buildApp from './app';
+// const request = require('supertest');
 
 process.env.GITHUB_TOKEN = 'fake';
 
 describe('Express app', () => {
-  const app = buildApp();
+  // const app = buildApp();
   describe('/api/repositories endpoint', () => {
     it.todo('should return an array');
   });
