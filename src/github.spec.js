043724bd7bc796ea602aa100f6d781a9f6d83b7b
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-04 12:12:33 +0300

    Clean-up.

--- src/github.spec.js
+++ src/github.spec.js
@@ -4,8 +4,7 @@ import fetchGitHubRepos from './github';
 process.env.GITHUB_TOKEN = 'fake';
 
 describe.skip('Fetching GitHub repositories', () => {
-  it.todo('returns an array of repositories');
-  it.todo('transforms repositories correctly');
-  it.todo('filters forks away');
-  it.todo('filters forks away');
+  it.todo('should return an array of repositories');
+  it.todo('should transform repositories as expected');
+  it.todo('should not return private repositories');
 });
