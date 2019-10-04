a9792e30266a631a1825a8a15432912d62c571bc
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-04 11:13:08 +0300

    Add react preset.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  presets: [[
-    '@babel/preset-env',
-    {
-      targets: {
-        node: 'current',
+  presets: [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
       },
-    },
-  ]],
+    ],
+  ],
 };
