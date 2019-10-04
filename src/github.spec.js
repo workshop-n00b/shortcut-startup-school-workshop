b714c83dd838a0d309de282774588ad392afb2fe
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-04 11:08:16 +0300

    Add a test for request path.

--- src/github.spec.js
+++ src/github.spec.js
@@ -1,5 +1,5 @@
 /* global describe, beforeAll, afterAll, beforeEach, it, expect */
-import unmock, { transform } from 'unmock';
+import unmock, { sinon, transform } from 'unmock';
 import fetchGitHubRepos from './github';
 
 process.env.GITHUB_TOKEN = 'fake';
@@ -22,9 +22,19 @@ describe('Fetching GitHub repositories', () => {
     githubv3.state(transform.withCodes(200));
   });
 
-  it('finds repositories', async () => {
+  afterEach(() => {
+    unmock.reset();
+  });
+
+  it('should return an array of repositories', async () => {
     const repos = await fetchGitHubRepos();
     expect(Array.isArray(repos)).toBe(true);
+  });
+  it('should call GitHub API with the correct path', async () => {
+    await fetchGitHubRepos();
+    sinon.assert.calledOnce(githubv3.spy);
+    const requestPath = githubv3.spy.getRequestPath();
+    expect(requestPath).toBe('/user/repos');
   });
   it.todo('transforms repositories correctly');
   it.todo('filters forks away');
